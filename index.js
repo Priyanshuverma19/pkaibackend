08e422d6a1c6d4f624acff3c1b655d423beecb90
priyanshu verma <priyanshu.vermaa0@gmail.com>
2024-12-09 18:41:05 +0530

    cors fix

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors({
     origin:process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials:true,
 
 }))
