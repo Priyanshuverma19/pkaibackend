970ba91edff8f755bccd16b1adfaf590aea6cda5
priyanshu verma <priyanshu.vermaa0@gmail.com>
2024-12-10 23:20:11 +0530

    indexhtml

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ app.use((err, req, res, next) => {
   app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/", "index.html"));
 });
   
 
